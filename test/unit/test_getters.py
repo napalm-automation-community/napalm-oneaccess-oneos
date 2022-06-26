f49063c7256a6f8255f2911ce9e99ac398165bdc
Robin Guillat <robin.guillat@gmail.com>
2022-06-26 19:05:14 +1000

    Prepared pytest config
    Edited setup.cfg by removing llama (something to be fixed later)
    Started to try to run the test-cases but at the moment it's not working, more work to do

--- test/unit/test_getters.py
+++ test/unit/test_getters.py
@@ -9,3 +9,28 @@ import pytest
 @pytest.mark.usefixtures("set_device_parameters")
 class TestGetter(BaseTestGetters):
     """Test get_* methods."""
+
+
+    # def test_get_facts(self, test_case):
+    #     """Test get_facts method."""
+    #     print(os.getenv("NAPALM_TEST_MOCK"))
+        # facts = self.device.get_facts()
+        # assert helpers.test_model(models.facts, facts)
+        # assert True
+        # return facts
+
+    def test_get_interfaces_ip(self, test_case):
+        """Test get_interfaces_ip."""
+        get_interfaces_ip = self.device.get_interfaces_ip()
+        assert len(get_interfaces_ip) > 0
+
+        for interface, interface_details in get_interfaces_ip.items():
+            ipv4 = interface_details.get("ipv4", {})
+            ipv6 = interface_details.get("ipv6", {})
+            for ip, ip_details in ipv4.items():
+                assert helpers.test_model(models.interfaces_ip, ip_details)
+            for ip, ip_details in ipv6.items():
+                assert helpers.test_model(models.interfaces_ip, ip_details)
+    
+        return get_interfaces_ip
+        
